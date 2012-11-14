7bb4ef3c5bb636e4df4d4e3a8f3f7a77effe79aa
Kentaro Takiguchi <takiguchi0817@gmail.com>
2012-11-14 12:24:20 -0800

    アニメーション速度調整用のシークバーの追加

--- DailyMotion/src/com/rejasupotaro/dailymotion/ui/MainActivity.java
+++ DailyMotion/src/com/rejasupotaro/dailymotion/ui/MainActivity.java
@@ -10,10 +10,13 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class MainActivity extends Activity {
 
@@ -27,6 +30,25 @@ public class MainActivity extends Activity {
             
             public void onClick(View v) {
                 DailyMotionHelper.launchGallarey(MainActivity.this);
+            }
+        });
+        
+        SeekBar animationSpeedSeekBar = (SeekBar) findViewById(R.id.seekbar_animation_speed);
+        animationSpeedSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            // トラッキング開始時に呼び出されます
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Log.v("onStartTrackingTouch()",
+                    String.valueOf(seekBar.getProgress()));
+            }
+            // トラッキング中に呼び出されます
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+                Log.v("onProgressChanged()",
+                    String.valueOf(progress) + ", " + String.valueOf(fromTouch));
+            }
+            // トラッキング終了時に呼び出されます
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.v("onStopTrackingTouch()",
+                    String.valueOf(seekBar.getProgress()));
             }
         });
         
