6678382f0f8d420cb99a711ac082b38afa25fb2a
Kentaro Takiguchi <takiguchi0817@gmail.com>
2012-11-18 23:13:21 -0800

    レールズやめた

--- server/DailyMotion/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DailyMotion::Application.config.secret_token = '37e44bd4f44bf6700f35c1ab5985216099d2996026549489af1eb7a2f12b5db66bf60343653baf6d6f42fb651fe67e4783fa8b7dff78326589f60ed73b8bd4ea'
