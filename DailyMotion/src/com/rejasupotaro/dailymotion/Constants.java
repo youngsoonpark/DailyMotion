5c937ec71a36f25983080992705e95cb40d022b8
Kentaro Takiguchi <takiguchi0817@gmail.com>
2012-11-21 23:11:19 -0800

    Production環境と設定をわけた

--- DailyMotion/src/com/rejasupotaro/dailymotion/Constants.java
+++ DailyMotion/src/com/rejasupotaro/dailymotion/Constants.java
@@ -1,7 +1,8 @@
 package com.rejasupotaro.dailymotion;
 
 public final class Constants {
-    public static final String APP_SITE_URL = "http://192.168.3.6:3000";
-    public static final String API_GENERATE_GIF_URL = "http://192.168.3.6:3000/api/convert";
+    public static final boolean PRODUCTION = true;
+    public static final String APP_SITE_URL = PRODUCTION ? "rejasupotaro.com:3000" : "http://192.168.3.6:3000";
+    public static final String API_GENERATE_GIF_URL = APP_SITE_URL + "/api/convert";
 
 }
