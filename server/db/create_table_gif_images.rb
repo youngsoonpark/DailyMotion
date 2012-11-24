0a1c361e5ffbbffc6c31a4ddab6b323b44d1c180
Kentaro Takiguchi <takiguchi0817@gmail.com>
2012-11-23 19:48:46 -0800

    文字コードちゃんと書かないとこわい

--- server/db/create_table_gif_images.rb
+++ server/db/create_table_gif_images.rb
@@ -6,5 +6,6 @@ client.query("DROP TABLE IF EXISTS gif_images")
 client.query("CREATE TABLE IF NOT EXISTS gif_images(" +
              "id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY," +
              "title VARCHAR(64) NOT NULL," +
-             "image_url VARCHAR(64) NOT NULL);")
+             "image_url VARCHAR(64) NOT NULL) " +
+             "CHARSET 'utf8';")
 
