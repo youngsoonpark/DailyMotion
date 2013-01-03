1738bacb26ec47e5670fe5f8d33873bd2fbac253
Kentaro Takiguchi <takiguchi0817@gmail.com>
2013-01-03 22:11:33 +0900

    Refactor code of show like_on image

--- server/public/javascripts/detail.js
+++ server/public/javascripts/detail.js
@@ -22,9 +22,6 @@
       var title = json["title"];
       var likeCount = json["like_count"];
 
-      if (likeCount > 0) {
-        $(".gif_box_like").attr("src", "/images/like_on.png");
-      }
       $(".gif_box_like").click(function() {
         $(".gif_box_like").attr("src", "/images/like_on.png");
         $.ajax({
